9b585e7650681ef5e0611aa7d9ee69b4774fc778
Michael LaRoy <mike.laroy@gmail.com>
2025-09-29 09:52:10 -0700

    add post install script

---
+++ export/StarterKitPostInstall.php
@@ -0,0 +1,10 @@
+<?php
+
+class StarterKitPostInstall
+{
+    public function handle($console)
+    {
+        $console->line('🍻 🍻 🍻 Thanks for installing!');
+        $console->line('Consider supporting the creator: https://buymeacoffee.com/mikelaroy');
+    }
+}
